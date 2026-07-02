c7a5cfcb125be929028209307f6cde05c09ca988
FiffyD <fiffydaphnyee@gmail.com>
2026-07-02 15:06:20 +0800

    exercise

---
+++ labs/lab03/exercise2/lab-03-2.py
@@ -0,0 +1,5 @@
+print("Total nights : ", end='', flush=True)
+numNight = int(input())
+totalPayment = numNight * 250.0 + numNight * 250.0 * 0.15
+print("Total Payment [RM] : ", end='', flush=True)
+print(totalPayment)
