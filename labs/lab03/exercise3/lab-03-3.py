16aad0d71ccf626d33d67dc4c138090597d4783a
FiffyD <fiffydaphnyee@gmail.com>
2026-07-02 15:34:23 +0800

    exercise3

---
+++ labs/lab03/exercise3/lab-03-3.py
@@ -0,0 +1,9 @@
+kilobyte = float(input())
+megabyte = kilobyte / 1000
+gigabyte = megabyte / 1000
+terabyte = gigabyte / 1000
+petabyte = terabyte / 1000
+print(megabyte)
+print(gigabyte)
+print(terabyte)
+print(petabyte)
