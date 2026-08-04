1bf69c4a01856605d7a3f19bd9241ef711301264
TuanNyamuk <mfaddiansyah@gmail.com>
2026-08-04 10:14:29 +0800

    py

--- labs/lab05/exercise.py
+++ labs/lab05/exercise.py
@@ -1,23 +1,3 @@
-# Numeric data types
-age = 21                    # int (integer)
-height = 5.9               # float (floating-point number)
-temperature = -15.5        # float (can be negative)
-
-# String data type
-student_name = "Muhammad Ali"    # str (string)
-course_title = 'Python Programming'  # str (single or double quotes)
-description = """This is a multi-line
-string that spans several lines."""   # str (triple quotes)
-
-# Boolean data type
-is_active = True           # bool (boolean)
-has_submitted = False      # bool (boolean).
-
-# Special data type
-nothing = None             # NoneType (represents absence of value)
-
-print(type(age))
-print(type(temperature))
-print(type(student_name))
-print(type(is_active))
-print(type(nothing))
+# Let's see Python keywords
+import keyword
+print(keyword.kwlist)
