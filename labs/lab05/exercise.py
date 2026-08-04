13a0abe04a1a40d44ff96169b229a318c642c59a
TuanNyamuk <mfaddiansyah@gmail.com>
2026-08-04 10:41:58 +0800

    PY

--- labs/lab05/exercise.py
+++ labs/lab05/exercise.py
@@ -1,3 +1,8 @@
-# Let's see Python keywords
-import keyword
-print(keyword.kwlist)
+name = "Alice"
+age = 25
+
+print("My name is " + name + " and I am " + str(age) + " years old.")
+
+print(f"\nMy name is {name} and I am {age} years old.")
+
+print(f"\n{name.upper()} has {len(name)} letters")
