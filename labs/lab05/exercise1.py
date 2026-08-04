c3cc82f5f497bba9c5d49b38b54e8c886412de1b
TuanNyamuk <mfaddiansyah@gmail.com>
2026-08-04 10:50:31 +0800

    PY

---
+++ labs/lab05/exercise1.py
@@ -0,0 +1,16 @@
+#Asks the user for their name, age, and course code
+
+name = str(input("Name : "))
+age = str(input("Age : "))
+course = str(input("Course Code : "))
+
+#Stores this information in appropriately named variables
+
+#Displays the student information and shows the data types
+
+print (f"\n\nName : {name}")
+print (type(name))
+print (f"\nAge : {age}")
+print (type(age))
+print (f"\nCourse Code : {course}")
+print (type(course))
