872735a762dad2d512b5e406e8e5168d0e0dc4ca
TuanNyamuk <mfaddiansyah@gmail.com>
2026-09-01 11:25:49 +0800

    lab7

--- labs/lab07/exercise3/exercise3.py
+++ labs/lab07/exercise3/exercise3.py
@@ -3,7 +3,17 @@ price = float(input())
 quantity = int(input())
 member_answer = input()
 
+order_total = price * quantity
 
+if order_total >= 100 :
+    free_shipping = "True"
+elif order_total > 0 :
+    free_shipping = "False"
+
+if member_answer == "yes":
+    is_member = True
+elif member_answer == "no":
+    is_member = False
 
 print(name.upper())
 print(order_total)
