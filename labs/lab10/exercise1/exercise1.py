5999a251359b7310efb026f397b7e67a1b5e1229
TuanNyamuk <mfaddiansyah@gmail.com>
2026-09-22 10:12:32 +0800

    py

--- labs/lab10/exercise1/exercise1.py
+++ labs/lab10/exercise1/exercise1.py
@@ -1,13 +1,16 @@
 num_rounds = int(input())
+final_score = 0
+bonus = 0
 
 for i in range(num_rounds):
     score = int(input())
 
     if score > 100:
         bonus = score * (20/100)
-        final_score = final_score + score + bonus
     else:
-        final_score = final_score + score
+        bonus = 0
+        
+    final_score = final_score + score + bonus
 
 rounds_processed = num_rounds
 
