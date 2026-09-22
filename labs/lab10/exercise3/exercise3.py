0822a33705fa9b153f5351b9960cbc363e4c07f4
TuanNyamuk <mfaddiansyah@gmail.com>
2026-09-22 10:28:08 +0800

    py

--- labs/lab10/exercise3/exercise3.py
+++ labs/lab10/exercise3/exercise3.py
@@ -1,6 +1,11 @@
 target_points = int(input())
+total_points = 0
+rounds_played = 0
 
-
+while total_points < target_points:
+    rounds_played += 1
+    points = int(input())
+    total_points = total_points + points
 
 print(total_points)
 print(rounds_played)
